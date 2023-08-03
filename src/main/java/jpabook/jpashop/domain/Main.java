95afb5e0890f2f005095da065c8c6a5a3ef72a98
haein-side <kimhi1640@gmail.com>
2023-08-03 17:32:56 +0900

    JPA basic lecture start

---
+++ src/main/java/jpabook/jpashop/domain/Main.java
@@ -0,0 +1,30 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class Main {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+
+
+            tx.commit(); // 엔티티가 변경되었는지 JPA가 트랜잭션 커밋하는 시점에 체크하고 쿼리 날림
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
