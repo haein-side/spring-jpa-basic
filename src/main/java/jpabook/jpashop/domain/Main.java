ed7f24d6caa83d62bffc289d470ca179303aec17
haein-side <kimhi1640@gmail.com>
2023-08-03 20:35:18 +0900

    단방향 연관관계 매핑 적용

--- src/main/java/jpabook/jpashop/domain/Main.java
+++ src/main/java/jpabook/jpashop/domain/Main.java
@@ -15,19 +15,32 @@ public class Main {
         tx.begin();
 
         try {
+            // 저장
             Team team = new Team();
             team.setName("TeamA");
             em.persist(team);
 
             Member member = new Member();
             member.setName("member1");
-            member.setTeamId(team.getId());
+            member.setTeam(team); // PK 뽑아서 insert할 때 FK로 활
+
             em.persist(member);
+
+//            영속성 콘텍스트 지우고 쿼리 생성
+//            em.flush();
+//            em.clear();
             
             Member findMember = em.find(Member.class, member.getId());
 
-            Long findTeamId = findMember.getTeamId();
-            Team findTeam = em.find(Team.class, findTeamId);
+//            Long findTeamId = findMember.getTeamId();
+//            Team findTeam = em.find(Team.class, findTeamId);
+
+            Team findTeam = findMember.getTeam();
+            System.out.println("findTeam = " + findTeam.getName());
+
+            // 연관관계 수정
+//            Team newTeam = em.find(Team.class, 100L);
+//            findMember.setTeam(newTeam);
 
             tx.commit(); // 엔티티가 변경되었는지 JPA가 트랜잭션 커밋하는 시점에 체크하고 쿼리 날림
         } catch (Exception e) {
