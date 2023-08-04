d53178ec0cabc1424980c70608861ef01c3b23ca
haein-side <kimhi1640@gmail.com>
2023-08-04 14:20:30 +0900

    연관관계 매핑 실습 - 단방향 관계 매핑

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -10,8 +10,20 @@ public class OrderItem {
     @Column(name = "ORDER_ITEM_ID")
     private Long id;
 
-    @Column(name = "MEMBER_ID")
-    private Long orderId;
+//    @Column(name = "MEMBER_ID")
+//    private Long orderId;
+
+    @ManyToOne
+    @JoinColumn(name = "ORDER_ID")
+    private Order order;
+
+//    @Column(name = "ITEM_ID")
+//    private Long itemId;
+
+    @ManyToOne
+    @JoinColumn(name = "ITEM_ID")
+    private Item item;
+
 
     private LocalDateTime orderDate;
 
@@ -24,14 +36,6 @@ public class OrderItem {
 
     public void setId(Long id) {
         this.id = id;
-    }
-
-    public Long getOrderId() {
-        return orderId;
-    }
-
-    public void setOrderId(Long orderId) {
-        this.orderId = orderId;
     }
 
     public LocalDateTime getOrderDate() {
@@ -48,5 +52,21 @@ public class OrderItem {
 
     public void setStatus(OrderStatus status) {
         this.status = status;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
     }
 }
