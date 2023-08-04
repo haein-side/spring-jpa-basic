c88f878f78cbff6408781f9357725ee8de2a7e64
haein-side <kimhi1640@gmail.com>
2023-08-04 13:17:15 +0900

    양방향 매핑 시 역방향에만 연관관계 설정하는 실수

--- src/main/java/jpabook/jpashop/domain/Main.java
+++ src/main/java/jpabook/jpashop/domain/Main.java
@@ -19,26 +19,19 @@ public class Main {
             // 저장
             Team team = new Team();
             team.setName("TeamA");
+//            team.getMembers().add(member);
             em.persist(team);
 
             Member member = new Member();
             member.setName("member1");
-            member.setTeam(team); // PK 뽑아서 insert할 때 FK로 활
-
+            member.setTeam(team); // Member.team 연관관계 맵핑
             em.persist(member);
+
+            team.getMembers().add(member); // JPA에서 안 읽긴 함
 
             // DB에 데이터 반영하고 영속성 컨텍스트 지움
             em.flush();
             em.clear();
-
-            // em.find() 호출 시 영속성 컨텍스트에 없어서 db에서 조회 -> 조회 쿼리 볼 수 있음
-            Member findMember = em.find(Member.class, member.getId());
-            List<Member> members = findMember.getTeam().getMembers();
-
-            for (Member m : members) {
-                System.out.println("m = " + m.getName());
-                System.out.println("===========");
-            }
 
             tx.commit(); // 엔티티가 변경되었는지 JPA가 트랜잭션 커밋하는 시점에 체크하고 쿼리 날림
         } catch (Exception e) {
