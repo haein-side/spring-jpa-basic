a58f1cf9c47b4a80b728e7b4d0e699144c8f99ab
haein-side <kimhi1640@gmail.com>
2023-08-08 01:22:22 +0900

    mapped super class

--- src/main/java/jpabook/jpashop/domain/BaseEntity.java
+++ src/main/java/jpabook/jpashop/domain/BaseEntity.java
@@ -3,8 +3,8 @@ package jpabook.jpashop.domain;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
-@MappedSuperclass // 매핑 정보만 받는 슈퍼클래스 (실제 엔티티는 아니고 extends 받는 모든 클래스에서 가짐)
-public class BaseEntity {
+@MappedSuperclass // 매핑 정보만 받는 슈퍼클래스 (실제 엔티티는 아니고 extends 받는 모든 클래스에서 가짐) - 운영을 위해 필요
+public abstract class BaseEntity {
     private String createdBy;
     private LocalDateTime createdDate;
     private String lastModifiedBy;
