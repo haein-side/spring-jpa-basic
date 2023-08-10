da371904d7accdffe1b6ac48e5ba1755325d4eb6
haein-side <kimhi1640@gmail.com>
2023-08-10 17:23:05 +0900

    임베디드 타입 : 객체지향스럽게, 응집력 있게 Entity 만들기

--- src/main/java/jpabook/jpashop/domain/Main.java
+++ src/main/java/jpabook/jpashop/domain/Main.java
@@ -18,21 +18,6 @@ public class Main {
         tx.begin();
 
         try {
-            Child child1 = new Child();
-            Child child2 = new Child();
-
-            Parent parent = new Parent();
-            parent.addChild(child1);
-            parent.addChild(child2);
-
-            em.persist(parent); // 부모 저장할 때 자녀 없어도 em.persist(child1); em.persist(child2); 하는 것처럼 -> CASCADE
-
-            em.flush();
-            em.clear();
-
-            Parent findParent = em.find(Parent.class, parent.getId());
-//            findParent.getChildList().remove(0);
-            em.remove(findParent);
 
             tx.commit(); // 엔티티가 변경되었는지 JPA가 트랜잭션 커밋하는 시점에 체크하고 쿼리 날림
         } catch (Exception e) {
--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,65 +17,12 @@ public class Member extends BaseEntity{
     @Column(name = "USERNAME") // @Column(name = "") Mapping할 Column의 이름을 지정.
     private String name;
 
-    // 즉시 로딩 : 즉시 조인 즉, 프록시 쓴 게 아님! 둘 다 그냥 실제 Entity임
-    @ManyToOne(fetch = LAZY)
-    @JoinColumn
-    private Team team;
+    // 기간 Period
+    @Embedded
+    private Period workPeriod;
 
-    @Column(name = "CITY")
-    private String city;
+    @Embedded
+    // 주소 Address
+    private Address homeAddress;
 
-    @Column(name = "STREET")
-    private String street;
-
-    @Column(name = "ZIPCODE")
-    private String zipCode;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public String getZipCode() {
-        return zipCode;
-    }
-
-    public void setZipCode(String zipCode) {
-        this.zipCode = zipCode;
-    }
-
-    public Team getTeam() {
-        return team;
-    }
-
-    public void setTeam(Team team) {
-        this.team = team;
-    }
 }
