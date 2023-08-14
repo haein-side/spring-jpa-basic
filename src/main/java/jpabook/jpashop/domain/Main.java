17894536b4fff8843000c561ca20df2d79267a4b
haein-side <kimhi1640@gmail.com>
2023-08-14 13:21:04 +0900

    프로젝션 : 영속성 컨텍스트에서 관리됨

--- src/main/java/jpabook/jpashop/domain/Main.java
+++ src/main/java/jpabook/jpashop/domain/Main.java
@@ -18,28 +18,20 @@ public class Main {
         tx.begin();
 
         try {
-
             Member_j member = new Member_j();
-            member.setUsername("member_jpql");
+            member.setUsername("member_hi");
+            member.setAge(10);
             em.persist(member);
 
-            // 반환타입 설정 가능 -> TypeQuery
-            // 기본적으로 엔티티 설정
-            TypedQuery<Member_j> query = em.createQuery("select m from Member_j m", Member_j.class);
+            em.flush();
+            em.clear();
 
-            List<Member_j> resultList = query.getResultList();
-
-            for (Member_j member_j : resultList) {
-                System.out.println("members = " + member_j);
-            }
-
-            List<Member_j> resultList2 = em.createQuery("select m from Member_j m where m.username = : username", Member_j.class)
-                    .setParameter("username", "member_jpql")
+            // 영속성 컨텍스트에서 모두 관리됨
+            List<Member_j> result = em.createQuery("select m from Member_j m", Member_j.class)
                     .getResultList();
 
-            for (Member_j member_j : resultList2) {
-                System.out.println("members_2 = " + member_j);
-            }
+            Member_j findMember = result.get(0);
+            findMember.setAge(20); // 수정이 되면 영속성 컨텍스트에서 관리가 되고 안 되면 관리 안 됨
 
             tx.commit(); // 엔티티가 변경되었는지 JPA가 트랜잭션 커밋하는 시점에 체크하고 쿼리 날림
         } catch (Exception e) {
