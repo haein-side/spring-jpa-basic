a8d21ee6c365e59324fe0f9b1dc48683f416b73a
haein-side <kimhi1640@gmail.com>
2023-08-14 13:26:32 +0900

    명시적 join, 묵시적 join

--- src/main/java/jpabook/jpashop/domain/Main.java
+++ src/main/java/jpabook/jpashop/domain/Main.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain;
 
 import jpa.jpa.shop.jpql.Member_j;
+import jpa.jpa.shop.jpql.Team_j;
 import org.hibernate.Hibernate;
 
 import javax.persistence.*;
@@ -32,6 +33,18 @@ public class Main {
 
             Member_j findMember = result.get(0);
             findMember.setAge(20); // 수정이 되면 영속성 컨텍스트에서 관리가 되고 안 되면 관리 안 됨
+
+            // 묵시적 조인
+            // Member 테이블과 Team 테이블이 inner join 되어서 쿼리 날라감
+            List<Team_j> result2 = em.createQuery("select m.team from Member_j m", Team_j.class)
+                    .getResultList();
+
+            // 명시적 조인
+            // 위와 같이 작성하면 안 됨
+            // 최대한 SQL문과 비슷하게 작성해야 튜닝 등 할 수 있으므로 더 좋음!
+            // JOIN 명시적으로 작성할 것
+            List<Team_j> result3 = em.createQuery("select t from Member_j m join m.team t", Team_j.class)
+                    .getResultList();
 
             tx.commit(); // 엔티티가 변경되었는지 JPA가 트랜잭션 커밋하는 시점에 체크하고 쿼리 날림
         } catch (Exception e) {
